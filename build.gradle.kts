1e5588faec222a220ccff810125ff9f06e41be40
Muhammad Faisal Amir <faisalamircs@gmail.com>
2021-12-23 13:13:14 +0700

    add: request API dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,18 @@ repositories {
 dependencies {
     testImplementation(kotlin("test"))
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0-RC3")
+    implementation("com.google.code.gson:gson:2.8.9")
+
+    implementation("com.squareup.okhttp3:okhttp:5.0.0-alpha.2")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.9.3")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.retrofit2:adapter-rxjava:2.9.0")
+    implementation("com.squareup.retrofit2:adapter-rxjava2:2.9.0")
+
+    implementation("io.insert-koin:koin-core:3.1.4") // Koin core features
+
+    implementation("io.reactivex.rxjava2:rxjava:2.2.21")
 }
 
 tasks.test {
